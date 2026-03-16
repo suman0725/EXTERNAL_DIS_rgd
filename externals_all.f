0e420915b55aa5ff3ac54c0ab30b3c2d19b59bf7
Suman Shrestha <suman@ifarm2401.jlab.org>
2026-03-16 12:14:39 -0400

    update

--- externals_all.f
+++ externals_all.f
@@ -421,7 +421,7 @@ cc also, all deltae_cc's are computed for Z-1, not Z!/-*
                  deltae_cc = 0.0199
               elseif(iA.eq.208) then !Lead	  
                  deltae_cc = 0.02035
-              elseif(iA.eq.119) then !Tin (natural, avgA=118.71)
+              elseif(iA.eq.118) then !Tin (natural, avgA=118.71)
                  deltae_cc = 0.01440
               endif
    
@@ -2113,7 +2113,7 @@ cc also, all deltae_cc's are computed for Z-1, not Z!/-*
       elseif(iA.eq.208) then !Lead        
            deltae_cc = 0.02035
 
-      elseif(iA.eq.119) then !Tin (natural Sn, avgA=118.71)
+      elseif(iA.eq.118) then !Tin (natural Sn, avgA=118.71)
            deltae_cc = 0.01440
 	endif
 
@@ -3118,7 +3118,7 @@ C=======================================================================
 ! from H. de Vries: Nuclear Charge Density Distributions from Elastic Electron
 !   Scattering. in Atomic Data and Nuclear Data Tables 36, 495(1987)
 ! 8/9/96
-      IF(IA.EQ.119)RADIUS=4.626    ! Sn-119/120, rms charge radius from de Vries 1987
+      IF(IA.EQ.118)RADIUS=4.626    ! Sn-119/120, rms charge radius from de Vries 1987
       IF(IA.EQ.205)RADIUS=5.470
       IF(IA.EQ.56) RADIUS=3.729    
       If(IA.EQ.28) RADIUS=3.085
